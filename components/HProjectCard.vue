06d91d498c0defc420a7aa950fdcb6431af5f394
Jenn Greiner <jenn.greiner1@gmail.com>
2025-10-24 15:03:58 -0600

    cards

---
+++ components/HProjectCard.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-card :color="color">
+        <div class="d-flex">
+            <v-img class="align-center" width="100px" :src="image"></v-img>
+            <div class="d-flex flex-column">
+                <v-card-title>{{ title }}</v-card-title>
+                <v-card-text>{{ description }}</v-card-text>
+            </div>
+        </div>
+
+    </v-card>
+</template>
+
+<script setup lang="ts">
+defineProps({
+    title: String,
+    description: String,
+    image: String,
+    color: String
+})
+</script>
---
+++ components/ProjectCard.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-card :color="color" max-width="45%">
+        <v-img class="align-end text-white" height="200" :src="image" cover></v-img>
+        <v-card-title>{{ title }}</v-card-title>
+        <v-card-text>{{ description }}</v-card-text>
+    </v-card>
+</template>
+
+<script setup lang="ts">
+defineProps({
+    title: String,
+    description: String,
+    image: String,
+    color: String
+})
+</script>
